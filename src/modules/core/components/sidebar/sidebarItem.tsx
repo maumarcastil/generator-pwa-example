4a302d5e78c5caa52a5d7ad53b78c8032bc936b7
mauricio martinez <maumarcastil@gmail.com>
2024-07-18 22:06:45 -0500

    feat: Add SidebarItem component for sidebar navigation

---
+++ src/modules/core/components/sidebar/sidebarItem.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { SidebarContext } from './sidebarContext'
+
+import { cn } from '../../utils/cn'
+
+interface SidebarItemProps {
+  text: string
+  icon: React.ReactNode
+  alert?: boolean
+  active?: boolean
+  path: string
+}
+
+export const SidebarItem = (props: SidebarItemProps) => {
+  const { text, icon, alert, active, path } = props
+
+  const { expanded } = React.useContext(SidebarContext)
+
+  return (
+    <>
+      <Link to={path}>
+        <li
+          className={cn(
+            'relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group',
+            {
+              'bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800':
+                active,
+              'hover:bg-indigo-50 text-gray-600': !active,
+            },
+          )}
+        >
+          {icon}
+          <span
+            className={cn('overflow-hidden transition-all', {
+              'w-52 ml-3': expanded,
+              'w-0': !expanded,
+            })}
+          >
+            {text}
+          </span>
+          {alert && (
+            <div
+              className={cn('absolute right-2 w-2 h-2 rounded bg-indigo-400', {
+                'top-2': !expanded,
+              })}
+            />
+          )}
+
+          {!expanded && (
+            <div
+              className={cn(
+                'absolute left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-indigo-800 text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-0',
+              )}
+            >
+              <span>{text}</span>
+            </div>
+          )}
+        </li>
+      </Link>
+    </>
+  )
+}
