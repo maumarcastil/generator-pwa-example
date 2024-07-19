8bf3e3efcc49ff5bb18f3f55847bebd2897ee852
mauricio martinez <maumarcastil@gmail.com>
2024-07-18 23:17:57 -0500

    feat: Create Sidebar component for sidebar navigation

---
+++ src/modules/core/components/sidebar/sidebar.tsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import { FiChevronLeft, FiChevronRight, FiMoreVertical } from 'react-icons/fi'
+
+import { SidebarContext } from '../../context/sidebarContext'
+
+import { cn } from '../../utils/cn'
+import LogoArli from '../../../../assets/LogoArli.png'
+
+interface SidebarProps {
+  children: React.ReactNode
+}
+
+export const Sidebar = ({ children }: SidebarProps) => {
+  const { expanded, setExpanded } = React.useContext(SidebarContext)
+
+  const handleToggle = () => {
+    setExpanded((prev) => !prev)
+  }
+
+  return (
+    <>
+      <aside className="h-screen">
+        <nav className="h-full flex flex-col bg-white border-r shadow-sm">
+          <div className="p-3 pb-2 flex justify-between items-center">
+            <img
+              alt="logo"
+              src={LogoArli}
+              className={cn(
+                'overflow-hidden transition-all object-contain max-h-[32px]',
+                {
+                  'w-28': expanded,
+                  'w-0': !expanded,
+                },
+              )}
+            />
+            <button
+              onClick={handleToggle}
+              className="py-2 px-3 rounded-lg bg-gray-50 hover:bg-gray-100 transition-colors"
+            >
+              {expanded ? (
+                <FiChevronLeft size={20} />
+              ) : (
+                <FiChevronRight size={20} />
+              )}
+            </button>
+          </div>
+
+          {/* Sidebar provider */}
+
+          <ul className="flex-1 px-3">{children}</ul>
+
+          <div className="border-t flex p-3 items-center">
+            {/* avatar */}
+            <img
+              alt="avatar"
+              className="w-10 h-10 rounded-md"
+              src="https://ui-avatars.com/api/?name=John+Doe&background=random"
+            />
+
+            {/* data user */}
+            <div
+              className={cn(
+                `flex justify-between items-center overflow-hidden transition-all`,
+                {
+                  'w-52 ml-3': expanded,
+                  'w-0': !expanded,
+                },
+              )}
+            >
+              <div className="leading-4">
+                <h4 className="font-semibold capitalize">john doe</h4>
+                <span className="text-xs  text-gray-600 ">
+                  johndoe@gmail.com
+                </span>
+              </div>
+              {/* more options */}
+              <FiMoreVertical size={20} />
+            </div>
+          </div>
+        </nav>
+      </aside>
+    </>
+  )
+}
