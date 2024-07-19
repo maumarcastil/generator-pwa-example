e97d2fdbf97616a17e5b4fbd754db258a3ed8e06
mauricio martinez <maumarcastil@gmail.com>
2024-07-18 23:17:45 -0500

    refactor: Update sidebarItem import in sidebar component

--- src/modules/core/components/sidebar/sidebarItem.tsx
+++ src/modules/core/components/sidebar/sidebarItem.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import { SidebarContext } from './sidebarContext'
+import { SidebarContext } from '../../context/sidebarContext'
 
 import { cn } from '../../utils/cn'
 
