695ee3d41973e836fcbe250117a97510ea11e17f
mauricio <maumarcastil@gmail.com>
2024-07-22 13:42:45 -0500

    chore: Update npm dependencies and add Capacitor plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ const withMT = require('@material-tailwind/react/utils/withMT')
 
 /** @type {import('tailwindcss').Config} */
 export default withMT({
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
