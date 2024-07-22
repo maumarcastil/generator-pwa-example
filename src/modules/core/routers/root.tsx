b2719005e78b5f109c9661ac77e132339f5668f4
mauricio <maumarcastil@gmail.com>
2024-07-22 13:50:00 -0500

    feat: Add RootLayout component and update routing in root.tsx

--- src/modules/core/routers/root.tsx
+++ src/modules/core/routers/root.tsx
@@ -6,76 +6,82 @@ import { PrivateRoute } from '../guards/privateGuard'
 
 import HomePage from '../../home/pages'
 
+import { RootLayout } from '../layout/rootLayout'
 import { DashboardLayout } from '../layout/dashboard'
 
 import { SidebarProvider } from '../context/sidebarContext'
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Navigate to="/auth" />,
-  },
-  {
-    path: '/auth',
-    element: <AuthRoute />,
+    element: <RootLayout />,
     children: [
       {
-        path: '',
-        element: <Navigate to="login" />,
+        path: '/',
+        element: <Navigate to="/auth" />,
       },
       {
-        path: 'login',
-        element: <div>Login</div>,
+        path: '/auth',
+        element: <AuthRoute />,
+        children: [
+          {
+            path: '',
+            element: <Navigate to="login" />,
+          },
+          {
+            path: 'login',
+            element: <div>Login</div>,
+          },
+          {
+            path: 'register',
+            element: <div>Register</div>,
+          },
+        ],
       },
       {
-        path: 'register',
-        element: <div>Register</div>,
-      },
-    ],
-  },
-  {
-    path: '/dashboard',
-    element: (
-      <>
-        <PrivateRoute>
-          <SidebarProvider>
-            <DashboardLayout />
-          </SidebarProvider>
-        </PrivateRoute>
-      </>
-    ),
-    children: [
-      {
-        path: '',
-        element: <HomePage />,
-      },
-      {
-        path: 'collections',
-        element: <div>Collections</div>,
-      },
-      {
-        path: 'marketplace',
-        element: <div>Marketplace</div>,
-      },
-      {
-        path: 'wallet',
-        element: <div>Wallet</div>,
-      },
-      {
-        path: 'materials',
-        element: <div>Materials</div>,
-      },
-      {
-        path: 'certificates',
-        element: <div>Certificates</div>,
-      },
-      {
-        path: 'settings',
-        element: <div>Settings</div>,
-      },
-      {
-        path: 'help',
-        element: <div>Help</div>,
+        path: '/dashboard',
+        element: (
+          <>
+            <PrivateRoute>
+              <SidebarProvider>
+                <DashboardLayout />
+              </SidebarProvider>
+            </PrivateRoute>
+          </>
+        ),
+        children: [
+          {
+            path: '',
+            element: <HomePage />,
+          },
+          {
+            path: 'collections',
+            element: <div>Collections</div>,
+          },
+          {
+            path: 'marketplace',
+            element: <div>Marketplace</div>,
+          },
+          {
+            path: 'wallet',
+            element: <div>Wallet</div>,
+          },
+          {
+            path: 'materials',
+            element: <div>Materials</div>,
+          },
+          {
+            path: 'certificates',
+            element: <div>Certificates</div>,
+          },
+          {
+            path: 'settings',
+            element: <div>Settings</div>,
+          },
+          {
+            path: 'help',
+            element: <div>Help</div>,
+          },
+        ],
       },
     ],
   },
