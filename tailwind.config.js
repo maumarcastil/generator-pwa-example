2e3fa2e26fe7bb18267d3bd1c58865872a433b13
mauricio <maumarcastil@gmail.com>
2024-07-22 11:07:29 -0500

    refactor: Update dependencies and configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line no-undef
+const withMT = require('@material-tailwind/react/utils/withMT')
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+export default withMT({
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    /* VitePWA({
+    VitePWA({
       registerType: 'autoUpdate',
       manifest: {
         name: 'Arli Generator',
@@ -40,6 +41,6 @@ export default defineConfig({
           },
         ],
       },
-    }), */
+    }),
   ],
 })
