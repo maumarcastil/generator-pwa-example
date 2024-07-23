6d9b1510cafd8abbdd500aa6102c5515ed5b26f8
mauricio <maumarcastil@gmail.com>
2024-07-23 12:07:55 -0500

    chore: Remove unused react-spring-bottom-sheet CSS import

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
 import './modules/core/theme/index.css'
-import 'react-spring-bottom-sheet/dist/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
