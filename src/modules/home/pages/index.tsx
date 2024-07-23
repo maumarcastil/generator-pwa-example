cb8d743516e0cde9f888d0d9c2a5ed89ef404c95
mauricio <maumarcastil@gmail.com>
2024-07-23 09:58:12 -0500

    feat: Add LocalNotificationComponent and LocationExample components

--- src/modules/home/pages/index.tsx
+++ src/modules/home/pages/index.tsx
@@ -1,10 +1,14 @@
 import { CameraExample } from '../../core/components/camera/CameraExample'
+import { LocationExample } from '../../core/components/location/locationExample'
+import { LocalNotificationComponent } from '../../core/components/notification/localNotifications'
 
 const HomePage = () => {
   return (
     <>
       <div>
         <CameraExample />
+        <LocalNotificationComponent />
+        <LocationExample />
       </div>
     </>
   )
